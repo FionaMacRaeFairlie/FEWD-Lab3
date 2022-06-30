f5f0cfe495fa3ae42014a83b8e2942454a2a2d9b
FionaMacRaeFairlie <88429943+FionaMacRaeFairlie@users.noreply.github.com>
2022-06-30 15:38:26 +0100

    files for FEWD-Lab3

--- src/data/data.js
+++ src/data/data.js
@@ -133,13 +133,5 @@ export const items = [
       price: "3",
       category: "side",
       available: "yes",
-    },
-    {
-      id: "18",
-      name: "sparkling mineral water",
-      description: "locally sourced",
-      price: "1.50",
-      category: "drinks",
-      available: "yes",
     }
   ];
